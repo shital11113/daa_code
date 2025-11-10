2ea950f0c8db694b4f05c144a4c7a8e45f2f54e8
shital11113 <wableshital277@gmail.com>
2025-11-10 10:23:35 +0530

    Add files via upload

---
+++ 3_Fractional_Knapsack.py
@@ -0,0 +1,17 @@
+arr = [[500,30]]
+w = 10
+price = 0
+
+arr = sorted(arr, key = lambda x : x[0] / x[1], reverse = True)
+
+for i in range(len(arr)):
+	itemWt = arr[i][1]
+	itemP = arr[i][0]
+	if(itemWt > w):
+		price += w*(itemP / itemWt)
+		break
+	else:
+		price += itemP	
+		w -= itemWt
+
+print(price)
